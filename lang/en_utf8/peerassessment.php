85d4b0fbaf57e00a72a8c0a52a74b45448f0a51a
Michael Hughes <michaelhughes@strath.ac.uk>
2010-10-14 09:31:24 +0000

    Reorganised strings in to alphabetical order for ease of updating.

--- lang/en_utf8/peerassessment.php
+++ lang/en_utf8/peerassessment.php
@@ -1,35 +1,38 @@
 <?php
 $string['modulename'] = 'Peer Assessment Activity';
 $string['modulenameplural'] = 'Peer Assessment Activities';
-$string['nogroup'] = 'You are not a member of a group.';
-$string['assignment'] = 'Assignment';
-$string['noassignment'] = 'No Assignment';
-$string['frequency'] ='Frequency';
-$string['availablefrom'] = "Available from";
-$string['submissiondate'] = 'Submission date';
-$string['oncefrequency'] = 'Once';
-$string['weeklyfrequency'] = 'Weekly';
-$string['unlimitedfrequency'] = 'Unlimited';
-$string['scheduling'] = 'Frequency';
-$string['canedit'] = 'Responses can be changed';
-$string['lowerbound'] ='Lower Bound';
-$string['upperbound'] ='Upper Bound';
-$string['lowerbound_help'] ='This is the value <strong>below</strong> which the average rating will be highlighted.';
-$string['upperbound_help'] ='This is the value <strong>above</strong> which the average rating will be highlighted.';
 
-$string['canedithelp'] = 'Allow students to change their responses.';
 $string['additionalinfo'] = '<p>This module <strong>requires</strong> the use of Groups!</p><p>If you have selected an <strong>Assignment</strong> then the groups that are specified for that assignment will be used.</p><p>If you <strong>haven\'t</strong> selected and assignment, you should choose a group option below.</p><p>It is probably easiest to create a \"Peer Assessment\" grouping to hold the groups that will be used.</p>';
-$string['notavailableyet']='This activity is not yet available.';
+$string['alreadycompleted'] = 'You have already completed this activity.';
+$string['assignment'] = 'Assignment';
+$string['availablefrom'] = "Available from";
+$string['canedit'] = 'Responses can be changed';
+$string['canedithelp'] = 'Allow students to change their responses.';
+$string['deleteratings'] ='Delete Student\'s Ratings';
 $string['expired']='This activity is no longer available (deadline passed).';
+$string['frequency'] ='Frequency';
+$string['lowerbound'] ='Lower Bound';
+$string['lowerbound_help'] ='This is the value <strong>below</strong> which the average rating will be highlighted.';
 $string['mustbestudent']='<p>You cannot perform this activity as staff, you must be in a student role.</p><p><a href=\'report.php?id=$a\'>View report</p>';
-$string['nogroups']='This doesn\'t appear to have a group set up';              
+$string['noassignment'] = 'No Assignment';
+$string['nogroup'] = 'You are not a member of a group.';
+$string['nogroups']='This doesn\'t appear to have a group set up';
+$string['nomembersfound'] = 'No members found';
+$string['notavailableyet']='This activity is not yet available.';
+$string['notenoughtimepassed'] = 'You can\'t complete this activity just now, not enough time has passed since your last submission.';
+$string['oncefrequency'] = 'Once';
 $string['peerassessmentactivityheading'] = 'Peer Assessment Activity: $a->peerassessment->name';
 $string['peerassessmentactivityheadingforgroup'] = 'Peer Assessment Activity: $a->peerassessmentname for Group $a->groupname';
-$string['alreadycompleted'] = 'You have already completed this activity.';
-$string['notenoughtimepassed'] = 'You can\'t complete this activity just now, not enough time has passed since your last submission.';
-$string['nomembersfound'] = 'No members found';
-$string['viewreport'] ='View Report';
 $string['peerassessmentreportheading'] = 'Peer Assessment Activity Report: $a->name';
-
+$string['recordrating'] ='Record a Rating';
+$string['scheduling'] = 'Frequency';
+$string['submissiondate'] = 'Submission date';
+$string['unlimitedfrequency'] = 'Unlimited';
+$string['upperbound'] ='Upper Bound';
+$string['upperbound_help'] ='This is the value <strong>above</strong> which the average rating will be highlighted.';
+$string['userpeerassessment'] ='Enable Peer Assessment Activity';
 $string['usernotactuallyingroup'] = 'You are not actually in this group. This has been recorded';
+$string['viewreport'] ='View Report';
+$string['weeklyfrequency'] = 'Weekly';
+
 ?>
