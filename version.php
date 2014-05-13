1227a29bfb68259732be9f607ef7f35208fbc8ec
Michael Aherne <michael.aherne@strath.ac.uk>
2014-05-13 10:50:49 +0000

    MYP-636. Updated version.php

--- version.php
+++ version.php
@@ -8,7 +8,8 @@
  * @author Michael Hughes
  * @author Viktor Dorfler
  */
-$module->version  = 2012080901;    //moodle 2.x should be 2011
+$module->version  = 2014051300;    //moodle 2.x should be 2011
 $module->requires = 2010112400;
+$module->maturity = MATURITY_ALPHA;
 $module->component = 'mod_peerassessment';
-$module->cron =0;
+$module->cron = 0;
