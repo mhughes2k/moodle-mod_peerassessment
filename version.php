49a61cd0314ea150d34bc1834e48d6a45f2a0bc0
Michael Aherne <michael.aherne@strath.ac.uk>
2015-02-05 14:41:45 +0000

    MYP-942. Changed $module to $plugin.

--- version.php
+++ version.php
@@ -8,8 +8,8 @@
  * @author Michael Hughes
  * @author Viktor Dorfler
  */
-$module->version  = 2014051300;    //moodle 2.x should be 2011
-$module->requires = 2010112400;
-$module->maturity = MATURITY_ALPHA;
-$module->component = 'mod_peerassessment';
-$module->cron = 0;
+$plugin->version  = 2014051300;    //moodle 2.x should be 2011
+$plugin->requires = 2010112400;
+$plugin->maturity = MATURITY_ALPHA;
+$plugin->component = 'mod_peerassessment';
+$plugin->cron = 0;
