2f061cec4c76b63cddbe6f0ac76c67d3a7442184
Michael Hughes <michaelhughes@strath.ac.uk>
2016-11-04 15:23:04 +0000

    Fixed white spacing

--- db/upgrade.php
+++ db/upgrade.php
@@ -56,34 +56,34 @@ function xmldb_peerassessment_upgrade($oldversion=0) {
      */
     if ($oldversion < 2016092305) {
     
-    	// Define field groupid to be added to peerassessment_ratings.
-    	$table = new xmldb_table('peerassessment_ratings');
-    	$field = new xmldb_field('groupid', XMLDB_TYPE_INTEGER, '11', null, XMLDB_NOTNULL, null, null, 'ratedby');
+        // Define field groupid to be added to peerassessment_ratings.
+        $table = new xmldb_table('peerassessment_ratings');
+        $field = new xmldb_field('groupid', XMLDB_TYPE_INTEGER, '11', null, XMLDB_NOTNULL, null, null, 'ratedby');
     
-    	// Conditionally launch add field groupid.
-    	if (!$dbman->field_exists($table, $field)) {
-    		$dbman->add_field($table, $field);
-    	}
+        // Conditionally launch add field groupid.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
 
-    	$table = new xmldb_table('peerassessment');
-    	$field = new xmldb_field('ratingscale', XMLDB_TYPE_INTEGER, '11', null, XMLDB_NOTNULL, null, 0, 'introformat');
-    	// Conditionally launch add field groupid.
-    	if (!$dbman->field_exists($table, $field)) {
-    		$dbman->add_field($table, $field);
-    	}
-    	$field = new xmldb_field('completionrating', XMLDB_TYPE_INTEGER, '1', null, null, null, 0, 'ratingscale');
-    	if (!$dbman->field_exists($table, $field)) {
-    		$dbman->add_field($table, $field);
-    	}
+        $table = new xmldb_table('peerassessment');
+        $field = new xmldb_field('ratingscale', XMLDB_TYPE_INTEGER, '11', null, XMLDB_NOTNULL, null, 0, 'introformat');
+        // Conditionally launch add field groupid.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+        $field = new xmldb_field('completionrating', XMLDB_TYPE_INTEGER, '1', null, null, null, 0, 'ratingscale');
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
    
-    	// Define field groupid to be added to peerassessment_ratings.
-    	$table = new xmldb_table('peerassessment_comments');
-    	$field = new xmldb_field('groupid', XMLDB_TYPE_INTEGER, '11', null, XMLDB_NOTNULL, null, null, 'studentcomment');
-    	// Conditionally launch add field groupid.
-    	if (!$dbman->field_exists($table, $field)) {
-    		$dbman->add_field($table, $field);
-    	}
-    	upgrade_mod_savepoint(true, 2016092305, 'peerassessment');
+        // Define field groupid to be added to peerassessment_ratings.
+        $table = new xmldb_table('peerassessment_comments');
+        $field = new xmldb_field('groupid', XMLDB_TYPE_INTEGER, '11', null, XMLDB_NOTNULL, null, null, 'studentcomment');
+        // Conditionally launch add field groupid.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+        upgrade_mod_savepoint(true, 2016092305, 'peerassessment');
     }
     return $result;
 
