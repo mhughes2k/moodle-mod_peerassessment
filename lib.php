85fd07acaa954cc271f0b39970cc048c29b4a10d
Michael Hughes <michaelhughes@strath.ac.uk>
2017-08-22 12:14:44 +0000

    Fixed STRATHPA-97

--- lib.php
+++ lib.php
@@ -80,11 +80,14 @@ function peerassessment_delete_instance($id) {
 	if (! $DB->delete_records('peerassessment_ratings', array('peerassessment'=>$data->id))) {
 		$result = false;
 	}
-	if ($events = $DB->get_records_select('event', "modulename = 'peerassessment' and instance = '{$data->id}'")) {
+
+	 if ($events = $DB->get_records_select('event', "modulename = 'peerassessment' and instance = '{$data->id}'")) {
 		foreach ($events as $event) {
-			delete_event($event->id);
+		    $ca = new calendar_event($event);
+		    $ca->delete();
 		}
 	}
+
 	//peerassessment_grade_item_delete($data);
 	return $result;
 }
